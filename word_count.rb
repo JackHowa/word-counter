df1be983559fce80cfc0b21ef53895ad45d8dd5e
jackhowa <johncarverhoward@gmail.com>
2017-08-28 17:34:13 -0500

    Sort by frequency and word alphabetically

--- word_count.rb
+++ word_count.rb
@@ -1,9 +1,3 @@
-
-
-
-# input: "hey hay! He"
-# output: ["hey", "hay", "he"]
-
 def cleaned_doc(word_string)
   word_string.split(" ").map do |word|
     word.gsub(/[[:punct:]]/, '').downcase()
@@ -11,46 +5,17 @@ def cleaned_doc(word_string)
 end
 
 
-
 def word_count_engine(word_string)
-  cleaned_doc(word_string)
-
-
-  outputArray = cleaned_doc(word_string).map do |word|
-    if output.keys.include?(word)
-      output[word] += 1
-    else
-      output[word] = 1
-    end
+  frequencies = Hash.new(0)
+  cleaned_doc(word_string).each do |word|
+      frequencies[word.downcase] += 1
   end
 
-  puts output
-
-  # tuples
-  rearrangedOutput = []
-  outputArray.each do |pair|
-    rearrangedOutput << [pair[1], pair[0]]
-  end
-  # [["practice", 3], ["makes", 1], ["perfect", 2], ["youll", 1], ["get", 1], ["by", 1], ["just", 4]]
-
-  sortedArray = rearrangedOutput.sort().reverse()
-
-  finalOutput = []
-  sortedArray.each do |pair|
-    finalOutput << [pair[1], pair[0].to_s]
+  sorted_hash = frequencies.sort_by do |word, frequency|
+    [frequency, word]
   end
 
-  finalOutput
-
-
-
-  # ["a", "b", "a"]
-  #
-
-  # [["practice"],["perfect"]]
-  # [["a", "2"], ["b", "1"]]
+  sorted_hash.reverse().map do |word, frequency|
+    [word, frequency.to_s]
+  end
 end
-
-input = "Practice makes perfect. you'll only get Perfect by practice. just practice!"
-
-word_count_engine(input)
